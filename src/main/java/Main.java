63224630a281491bd360e796a79ef80fcd196e7b
yahavg08 <yahavgoldshtein1108@gmail.com>
2024-09-15 11:32:22 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
-}
+    public static void main(String[] args) {
+        BankAccount bobAccount = new BankAccount(1000, -1000);
+        BankAccount lisaAccount = new BankAccount(1000, -1000);
+        System.out.println("Bob's balance: " + bobAccount.getBalance());
+        System.out.println("Lisa's balance: " + lisaAccount.getBalance());
+        bobAccount.transfer(lisaAccount, 1600);
+        System.out.println("Bob's balance after transfer attempt: " + bobAccount.getBalance());
+        System.out.println("Lisa's balance after transfer attempt: " + lisaAccount.getBalance());
+    }
