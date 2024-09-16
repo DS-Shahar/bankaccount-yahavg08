0c8d122f4d497caceebc47a77a04bcebd7245913
yahavg08 <yahavgoldshtein1108@gmail.com>
2024-09-16 21:30:42 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,15 @@
-class Main {
+package bankAccount;
+
+public class Main {
     public static void main(String[] args) {
-        BankAccount bobAccount = new BankAccount(1000, -1000);
-        BankAccount lisaAccount = new BankAccount(1000, -1000);
+        BankAccount bobAccount = new BankAccount(1000, -1000, "Bob");
+        BankAccount lisaAccount = new BankAccount(1000, -1000, "Lisa");
         System.out.println("Bob's balance: " + bobAccount.getBalance());
         System.out.println("Lisa's balance: " + lisaAccount.getBalance());
+        bobAccount.setBalance(1200);
+        System.out.println("Bob's balance after update: " + bobAccount.getBalance());
+        lisaAccount.deposit(500);
+        System.out.println("Lisa's balance after deposite=" + lisaAccount.getBalance());
         bobAccount.transfer(lisaAccount, 1600);
         System.out.println("Bob's balance after transfer attempt: " + bobAccount.getBalance());
         System.out.println("Lisa's balance after transfer attempt: " + lisaAccount.getBalance());
